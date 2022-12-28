ff105105f7f736808523344c2b4e0795018d0d9f
Mina Aminpour <mina.aminpur@gmail.com>
2022-12-29 00:41:48 +0330

    adding custom permision to the doctors endpoint

--- yasha/views.py
+++ yasha/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import DoctorSerializer
 from .models import Doctor
+from .permissions import IsAdminOrReadOnly
 
 
 class DoctorViewSet(ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    permission_classes = [IsAdminOrReadOnly]
     
