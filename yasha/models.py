4985205b6ea8cded57d70b9fde4dc2aaf3632146
Mina Aminpour <mina.aminpur@gmail.com>
2023-01-07 20:46:18 +0330

    editing the ServiceType model

--- yasha/models.py
+++ yasha/models.py
@@ -18,7 +18,7 @@ class Department(models.Model):
 
 class ServiceType(models.Model):
     name = models.CharField(("نوع خدمت"), max_length=150)
-    description = models.CharField(("توضیحات"), max_length=255, null=True, blank=True)
+    description = models.TextField(("توضیحات"), null=True, blank=True)
     last_update = models.DateTimeField(("آخرین بروزرسانی"), auto_now=True)
 
     def __str__(self):
