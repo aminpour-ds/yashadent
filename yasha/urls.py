8c50835b2ce922e869c2c03d22952ddab7e197b9
Mina Aminpour <mina.aminpur@gmail.com>
2023-01-07 15:46:34 +0330

    changeing and adding url's of endpoints

--- yasha/urls.py
+++ yasha/urls.py
@@ -12,8 +12,8 @@ router.register('about', views.AboutViewSet, basename='about')
 router.register('AppointmentRequest', views.AppointmentRequestViewSet)
 
 urlpatterns = [
-    path('api/servicetype/', views.ServiceTypeViewSet.as_view({'get': 'list'})),
-    path('api/servicetype/<int:pk>/', views.ServiceViewSet.as_view({'get': 'list'})),
+    path('api/service/', views.ServiceTypeViewSet.as_view({'get': 'list'})),
+    path('api/service/<int:pk>/', views.ServiceViewSet.as_view({'get': 'list'})),
     path('api/', include(router.urls)),
 ]
 
@@ -25,8 +25,9 @@ html_router.register('about', viewshtml.About, basename='about')
 html_router.register('', viewshtml.Index)
 
 urlpatterns += [
-    path('servicetype/', viewshtml.servicetype.as_view({'get': 'list'})),
-    path('servicetype/<int:pk>/', viewshtml.service.as_view({'get': 'list'})),
-    path('appointment', TemplateView.as_view(template_name='appointment.html')),
+    path('service/', viewshtml.servicetype.as_view({'get': 'list'})),
+    path('service/<int:pk>/', viewshtml.service.as_view({'get': 'list'})),
+    path('appointment/', TemplateView.as_view(template_name='appointment.html')),
+    path('contact/', TemplateView.as_view(template_name='contact.html')),
     path('', include(html_router.urls)),    
 ]
