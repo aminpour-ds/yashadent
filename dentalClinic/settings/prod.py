8d10e5dd548716ae982c50cb08e45af1b95adac1
Mina <mina.aminpur@gmail.com>
2023-03-04 15:50:31 +0330

    preparing production-2

--- dentalClinic/settings/prod.py
+++ dentalClinic/settings/prod.py
@@ -1,10 +1,14 @@
 from .common import *
 import os
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['192.168.1.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['yashadent.com/', 'yashadent.ir/']   # just domain name without https://
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
