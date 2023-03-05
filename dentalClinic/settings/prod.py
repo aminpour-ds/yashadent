47337acdb7cfaf245daa6307e7e705a038bc69ba
Mina <mina.aminpur@gmail.com>
2023-03-05 15:22:10 +0330

    preparing production step3

--- dentalClinic/settings/prod.py
+++ dentalClinic/settings/prod.py
@@ -1,14 +1,21 @@
 from .common import *
 import os
-import dj_database_url
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+ALLOWED_HOSTS = ['yashadent.ir', 'www.yashadent.ir', 'yashadent.com', 'www.yashadent.com']   # just domain name without https://
 
-ALLOWED_HOSTS = ['yashadent.com/', 'yashadent.ir/']   # just domain name without https://
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'), 
+        'USER': os.environ.get('DB_USER'), 
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
